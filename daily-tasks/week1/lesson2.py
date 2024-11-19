d29cbd0ec04379fe7f083e4d5421c382d9039d45
EstherOkiri <est.okiri@gmail.com>
2024-11-19 21:30:39 +0300

    lesson 2 coding challenges

---
+++ daily-tasks/week1/lesson2.py
@@ -0,0 +1,18 @@
+# Exercise 1
+username = input("Enter your name: ")
+
+print(f"Hello {username}")
+
+#Exercise 2
+hours = int(input("Number of hours: "))
+rate = int(input("Enter the rate per hour: "))
+gross_pay = hours * rate
+print(f"Gross pay is {gross_pay}.")
+
+#convert celsius to farenheit
+
+degree_celcius = float(input("Enter temperature in celcius: "))
+degree_farenheit = (degree_celcius * (9/5)) + 32
+
+print(f"Degree in Farenheit is {degree_farenheit}")
+
